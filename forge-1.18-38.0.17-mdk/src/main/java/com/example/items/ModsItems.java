4ceb266c43e3d0897cec611c21eb0cfc44e93cb9
Florian <florian.marteau@iut-dijon.u-bourgogne.fr>
2023-01-30 19:41:55 +0100

    Modification gestion items classe

--- forge-1.18-38.0.17-mdk/src/main/java/com/example/items/ModsItems.java
+++ forge-1.18-38.0.17-mdk/src/main/java/com/example/items/ModsItems.java
@@ -1,4 +1,4 @@
-package Item;
+package com.example.items;
 
 import com.example.bamboo.Bamboo;
 import net.minecraft.world.item.CreativeModeTab;
@@ -8,16 +8,14 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
-public class BanBook {
+public class ModsItems {
 
     // Creer nouveau item
     public static final DeferredRegister<Item> items = DeferredRegister.create(ForgeRegistries.ITEMS, Bamboo.ID_MOD);
 
-    // nom de l'item
-    private static String itemName = "bambook";
 
     // Création de l'item
-    public static final RegistryObject<Item> bambook = items.register(itemName,
+    public static final RegistryObject<Item> bambook = items.register("bambook",
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD))); // Créer l'item et l'ajoute à la liste des items de décoration
 
     public static void register(IEventBus eventBus){
